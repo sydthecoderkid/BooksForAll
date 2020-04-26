03da8a50c75676258d2762620298556ee58cf21d
sydneykeating <bookmasterdc@gmail.com>
2020-04-25 22:12:02 -0400

    Added in some updates for the book class.

---
+++ BooksForAll/CurrentBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using Xamarin.Forms;
+using System.Collections.Generic;
+namespace BooksForAll
+{
+    public class CurrentBook:Image
+    {
+        public string isbn = "9780605039070";
+        public string filelocation = "BookCover.jpg";
+       public int xposition;
+        public string bookname;
+        public string authorname;
+        public CurrentBook()
+        {
+            this.WidthRequest = 500;
+            this.HeightRequest = 500;
+            this.TranslateTo(8, 80);
+            this.Aspect = Aspect.AspectFit;
+            this.Source = ImageSource.FromFile(filelocation);
+
+        }
+    }
+}
