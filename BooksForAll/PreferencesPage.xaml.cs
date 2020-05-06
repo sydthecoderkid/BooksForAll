27e76d95273b84989125cc50d1bb5baa8935d3a9
sydthecoderkid <bookmasterdc@gmail.com>
2020-05-06 18:29:50 -0400

    Created a preferences page for race

--- BooksForAll/PreferencesPage.xaml.cs
+++ BooksForAll/PreferencesPage.xaml.cs
@@ -7,9 +7,84 @@ namespace BooksForAll
 {
     public partial class PreferencesPage : ContentPage
     {
+
+       
         public PreferencesPage()
         {
-            InitializeComponent();
+            Label header = new Label
+            {
+                Text = "ListView",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            // Define some data.
+            List<string> races = new List<string>
+            {
+               "Black",
+               "White",
+               "Asian"
+            };
+
+            // Create the ListView.
+            ListView listView = new ListView
+            {
+                // Source of data items.
+                ItemsSource = races,
+
+                // Define template for displaying each item.
+                // (Argument of DataTemplate constructor is called for 
+                //      each item; it must return a Cell derivative.)
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    // Create views with bindings for displaying each property.
+                    Label nameLabel = new Label();
+                    nameLabel.Text = "test";
+
+                  
+
+                    BoxView boxView = new BoxView();
+   
+
+                    // Return an assembled ViewCell.
+                    return new ViewCell
+                    {
+                        View = new StackLayout
+                        {
+                            Padding = new Thickness(0, 5),
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                                {
+                                    boxView,
+                                    new StackLayout
+                                    {
+                                        VerticalOptions = LayoutOptions.Center,
+                                        Spacing = 0,
+                                        Children =
+                                        {
+                                            nameLabel,
+                                           
+                                        }
+                                        }
+                                }
+                        }
+                    };
+                })
+            };
+
+            // Accomodate iPhone status bar.
+            this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    listView
+                }
+            };
         }
     }
 }
+        
