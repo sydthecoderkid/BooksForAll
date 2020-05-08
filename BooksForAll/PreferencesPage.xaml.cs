d092325ea44ab29672bc59c301475848c643705b
sydthecoderkid <bookmasterdc@gmail.com>
2020-05-08 12:44:42 -0400

    Updating UI

--- BooksForAll/PreferencesPage.xaml.cs
+++ BooksForAll/PreferencesPage.xaml.cs
@@ -19,11 +19,40 @@ namespace BooksForAll
                TranslationY = 100     //Higher number, lower text.
                 
             };
+
+            Label racedescription = new Label
+            {
+                Text = "I want to see books with ",
+                FontSize = 18,
+                HorizontalOptions = LayoutOptions.Center,
+                TranslationY = 250,   //Higher number, lower text
+                TranslationX = -80
+            };
+            Label characters = new Label
+            {
+                Text = "characters ",
+                FontSize = 18,
+                HorizontalOptions = LayoutOptions.Center,
+                TranslationY = 122,   //Higher number, lower text
+                TranslationX = 145.8
+            };
+
+            Label underscore = new Label
+            {
+                Text = "________",
+                FontSize = 18,
+                HorizontalOptions = LayoutOptions.Center,
+                TranslationY = 200,   //Higher number, lower text
+                TranslationX = 100
+            };
             this.Content = new StackLayout
             {
                 Children =
                 {
+                    racedescription,
                     header,
+                    underscore,
+                    characters
                     
                 }
             };
