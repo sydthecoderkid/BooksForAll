aed92bc0618fed58186959ec7f59537305597871
sydthecoderkid <bookmasterdc@gmail.com>
2020-05-08 14:26:57 -0400

    Added more options

--- BooksForAll/PreferencesPage.xaml.cs
+++ BooksForAll/PreferencesPage.xaml.cs
@@ -14,16 +14,17 @@ namespace BooksForAll
             Label header = new Label
             {
                 Text = "Preferences",
-                FontSize = 55,
+                FontSize = 45,
                 HorizontalOptions = LayoutOptions.Center,
-               TranslationY = 100     //Higher number, lower text.
+               TranslationY = 90     //Higher number, lower text.
                 
             };
 
             Label racedescription = new Label
             {
-                Text = "I want to see books with ",
-                FontSize = 18,
+                Text = "•I want to see books with ",
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 16,
                 HorizontalOptions = LayoutOptions.Center,
                 TranslationY = 250,   //Higher number, lower text
                 TranslationX = -80
@@ -31,20 +32,87 @@ namespace BooksForAll
             Label characters = new Label
             {
                 Text = "characters ",
-                FontSize = 18,
+                FontSize = 16,
+                FontAttributes = FontAttributes.Bold,
                 HorizontalOptions = LayoutOptions.Center,
                 TranslationY = 122,   //Higher number, lower text
-                TranslationX = 145.8
+                TranslationX = 142.8
             };
 
             Label underscore = new Label
             {
-                Text = "________",
+                Text = "_________",
                 FontSize = 18,
                 HorizontalOptions = LayoutOptions.Center,
-                TranslationY = 200,   //Higher number, lower text
-                TranslationX = 100
+                TranslationY = 150,   //Higher number, lower text
+                TranslationX = 58
             };
+
+            Label blacklabel = new Label
+            {
+                Text = "Black",
+                FontSize = 19,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center,
+                TranslationY = 145,   //Higher number, lower text
+                TranslationX = 45
+            };
+
+            Label asianlabel = new Label
+            {
+                Text = "Asian",
+                FontSize = 19,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center,
+                TranslationY = 100,   //Higher number, lower text
+                TranslationX = 45
+            };
+
+            Label latinolabel = new Label
+            {
+                Text = "Latino",
+                FontSize = 19,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center,
+                TranslationY = 55,   //Higher number, lower text
+                TranslationX = 48
+            };
+
+            CheckBox blackbox = new CheckBox
+            {
+                
+                TranslationX = 260,
+                TranslationY = 105
+                
+
+
+
+            };
+
+            CheckBox asianbox = new CheckBox
+            {
+
+                TranslationX = 260,
+                TranslationY = 60
+
+
+
+
+            };
+
+
+            CheckBox latinobox = new CheckBox
+            {
+
+                TranslationX = 260,
+                TranslationY = 16
+
+
+
+
+            };
+
+
             this.Content = new StackLayout
             {
                 Children =
@@ -52,8 +120,14 @@ namespace BooksForAll
                     racedescription,
                     header,
                     underscore,
-                    characters
-                    
+                    characters,
+                    blacklabel,
+                     blackbox,
+                     asianlabel,
+                     asianbox,
+                     latinolabel,
+                     latinobox
+
                 }
             };
 
