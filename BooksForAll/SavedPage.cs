5ade696c65424e5d5c6ad0510654389b5786323f
sydthecoderkid <bookmasterdc@gmail.com>
2020-06-18 23:16:25 -0400

    Added Saved and Suggestion Pages

---
+++ BooksForAll/SavedPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace BooksForAll
+{
+    public class SavedPage : ContentPage
+    {
+        public SavedPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ BooksForAll/SuggestionPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace BooksForAll
+{
+    public class SuggestionPage : ContentPage
+    {
+        public SuggestionPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
